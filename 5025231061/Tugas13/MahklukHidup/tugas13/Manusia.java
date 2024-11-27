1b4cb4dc5493835bada74641eb304f483584bdbe
Happpizh <162117394+Happpizh@users.noreply.github.com>
2024-11-27 19:58:19 +0700

    Delete 5025231061/Tugas13/MahklukHidup directory

--- 5025231061/Tugas13/MahklukHidup/tugas13/Manusia.java
+++
@@ -1,9 +0,0 @@
-public class Manusia extends MakhlukHidup {
-    private String duaKaki;
-    public Manusia(String duaKaki){
-        this.duaKaki = duaKaki;
-    }
-    public void berdiri (){
-        System.out.println("Manusia berdiri dengan :  "+duaKaki);
-    }
-}
